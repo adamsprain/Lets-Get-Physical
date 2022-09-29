4264e9d6f5bbdb8c3dfcebb2f659d129bd0d3ace
wesley_burnawan <wburnawan@wisc.edu>
2022-09-28 20:30:10 -0500

    Solved Wesley's issue

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 print("Hello, world!")
+<<<<<<< HEAD
 print("Leo Brunette")
 print("My name is Holly and I like potatoes")
+=======
+print("Hello, I'm Wesley!")
+>>>>>>> b49d12d (Added Wesley's print)
