1f67f0d77a1781bb82ed3c063d5f66236dd897b3
Sparravex <ajsprain@gmail.com>
2022-11-10 16:09:35 -0600

    Initializing mongoose to server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+const connection_string = process.env.ATLAS_URI
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+
 // get driver connection
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+  console.log("Server running on port " + port);
+})
 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+mongoose.connect(connection_string)
+.then(() => console.log("MongDB connection established..."))
+.catch((error) => console.error("MongoDB Connection Failed", error.message))
